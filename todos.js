f8232c7b0ddc67dfbbb41cc3626f8d257f012880
Paul Chung <cleanvessel@gmail.com>
2015-08-21 16:09:24 +0900

    Initial Version
    - Basic Settings

---
+++ todos.js
@@ -0,0 +1,14 @@
+Todos = new Meteor.Collection('todos');
+
+if (Meteor.isClient) {
+    Template.TodosPanel.helpers({
+        items: function() {
+            return Todos.find();
+        }
+    });
+
+}
+
+if (Meteor.isServer) {
+
+}
